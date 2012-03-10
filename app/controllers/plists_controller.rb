cb0d2d4d46868956ac6ca92ca0e08749aa3929f3
Nathan Witmer <nwitmer@gmail.com>
2012-03-10 14:08:41 -0700

    Persist plists in the database, using uuid as the identifier

--- app/controllers/plists_controller.rb
+++ app/controllers/plists_controller.rb
@@ -4,7 +4,17 @@ class PlistsController < ApplicationController
   end
 
   def create
-    plist = LaunchdPlist.new params[:plist]
+    @plist = LaunchdPlist.new params[:plist]
+
+    if @plist.save
+      redirect_to plist_path(@plist.uuid, :format => :xml)
+    else
+      render :action => :new
+    end
+  end
+
+  def show
+    plist = LaunchdPlist.find_by_uuid params[:id]
     render :xml => LaunchdSerializer.new(plist).to_plist
   end
 end
--- spec/controllers/plists_controller_spec.rb
+++ spec/controllers/plists_controller_spec.rb
@@ -2,18 +2,36 @@ require 'spec_helper'
 
 describe PlistsController do
   describe "POST to create" do
-    before do
+
+    def create(overrides={})
       post :create,
         :format => :xml,
         :plist => {
           :name => "test command",
           :command => "echo hello",
           :interval => "300"
-        }
+        }.merge(overrides)
     end
 
-    it "is successful" do
-      response.should be_success
+    it "redirects to the new plist path, using uuid as the id" do
+      create
+      plist = LaunchdPlist.last
+      response.should redirect_to(plist_path(plist.uuid, :format => :xml))
+    end
+
+    it "creates a launchd plist entry" do
+      lambda { create }.should change(LaunchdPlist, :count).by(1)
+    end
+  end
+
+  describe "GET to show with a UUID" do
+
+    let :plist do
+      LaunchdPlist.create! :name => "test", :command => "ls", :interval => "300"
+    end
+
+    before do
+      get :show, :id => plist.uuid
     end
 
     it "renders an xml plist" do
